16919ecbb0d420e387fbfa54ceb1312e11cbccfd
Rogelio Hiso <97689058+RoHiso@users.noreply.github.com>
2023-11-11 21:38:18 -0300

    conrtruccion login, signin, routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//componentes
+import { LoginComponent } from './components/login/login.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path:'',redirectTo: 'login', pathMatch: 'full'},
+  {path:'login', component:LoginComponent},
+  {path:'signin', component:SigninComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'**', redirectTo:'login', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
